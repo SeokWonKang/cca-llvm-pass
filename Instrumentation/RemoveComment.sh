c0d911ed92119a60829497cb7fa123d26e73aabf
SeokwonKang <kswon0202@gmail.com>
2025-02-25 13:34:35 +0900

    update RemoveComment.sh: enable to get compile options directly and extract output path to remove comment

--- Instrumentation/RemoveComment.sh
+++ Instrumentation/RemoveComment.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Usage: bash RemoveComment.sh <assembly>
 
-sed -e 's/\#removethiscomment //g' -i $1
+OUTPUT_PATH=0
+for arg in $@
+do
+	if [[ $OUTPUT_PATH != 0 ]] 
+	then 
+		sed -e 's/\#removethiscomment //g' -i $arg
+		break
+	fi
+	if [ $arg == '-o' ]; then OUTPUT_PATH=1; fi
+done
