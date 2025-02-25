6f581fbfa27411edafe0cf843b9e0339a98c1335
SeokwonKang <kswon0202@gmail.com>
2025-02-25 13:40:57 +0900

    update RemoveComment.sh: expand coverage of comment // in addition to #

--- Instrumentation/RemoveComment.sh
+++ Instrumentation/RemoveComment.sh
@@ -7,6 +7,7 @@ do
 	if [[ $OUTPUT_PATH != 0 ]] 
 	then 
 		sed -e 's/\#removethiscomment //g' -i $arg
+		sed -e 's/\/\/removethiscomment //g' -i $arg
 		break
 	fi
 	if [ $arg == '-o' ]; then OUTPUT_PATH=1; fi
