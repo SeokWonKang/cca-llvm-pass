92ace8106e7ed563771afec65798c5610ed75651
SeokwonKang <kswon0202@gmail.com>
2025-04-07 01:12:00 +0900

    diable cca 8

--- Instrumentation/PassPlugin.cpp
+++ Instrumentation/PassPlugin.cpp
@@ -10,8 +10,7 @@ PassPluginLibraryInfo getPassPluginInfo() {
 	const auto callback = [](PassBuilder &PB) {
 		PB.registerOptimizerLastEPCallback([&](ModulePassManager &MPM, auto) {
 			MPM.addPass(createModuleToFunctionPassAdaptor(cca::CCAUniversalPass("7: o24 = i24 + i25 + i26 + i27 + i28")));
-			MPM.addPass(
-				createModuleToFunctionPassAdaptor(cca::CCAUniversalPass("8: o24 = i24 + i28; o25 = i25 + o24; o26 = i26 + o25; o27 = i27 + o26")));
+			// MPM.addPass(createModuleToFunctionPassAdaptor(cca::CCAUniversalPass("8: o24 = i24 + i28; o25 = i25 + o24; o26 = i26 + o25; o27 = i27 + o26")));
 			MPM.addPass(createModuleToFunctionPassAdaptor(cca::CCAUniversalPass("9: t24 = i24 > i25 ? i24 : i25; o24 = t24 > i26 ? t24 : i26")));
 			return true;
 		});
